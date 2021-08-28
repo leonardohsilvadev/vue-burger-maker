66dfe3d543e7cb73c0ea6157fb0f37c2bb471b02
Leonardo <leonardo.silva@modalgr.com.br>
2021-08-27 23:58:07 -0300

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <!-- <router-link>
+      <img src="" id="logo">
+    </router-link> -->
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
